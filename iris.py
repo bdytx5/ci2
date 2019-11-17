a9f3ee355551340db84333334aea391f358c74ca
bdytx5 <bdytx5@mail.missouri.edu>
2019-11-17 15:47:36 -0600

    everything before triangular mem- SUNDAY

--- iris.py
+++ iris.py
@@ -7,51 +7,47 @@
 # membership functions 
 
 # membership function
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+fig = plt.figure(0)
+ax = plt.axes()
+ax.set_xlabel('Pedal Length (Small, Medium, and Large)')
+ax.set_ylabel('Degree of Membership')
 
-# fig = plt.figure(0)
-# ax = plt.axes()
-# ax.set_xlabel('Pedal Length')
-# ax.set_ylabel('Rule activation')
 
-
-lowPla = 0
-lowPlb = 1
 lowPlc = 1.9
 lowPld = 2.2
 
-# x = [lowPla,lowPlb,lowPlc,lowPld] 
-# y = [0,1,1,0] 
-# if not usingTriMem:
-#     plt.plot(x, y,color='orange') 
-
+x = [0,lowPlc,lowPld] 
+y = [1,1,0] 
+plt.plot(x, y,color='green') 
 def lowPl(x):
-    a = 0
-    b = 1
-    c = 1.9
-    d = 2.2
 
-    if x < a:
-        return 0
-    if x >= a and x <= b:
-        return (x - a)/(b - a)
-    if x >= b and x <= c:
+    c = lowPlc
+    d = lowPld
+
+    if x < c:
         return 1
     if x >= c and x <= d:
-        return (d - x)/(d - c)
+        return round((d - x)/(d - c),2)
     else:
         return 0
 
 medPla = 2
-medPlb = 3.9
+medPlb = 3.0
 medPlc = 4.1
-medPld = 6
+medPld = 5
 
+
+x = [medPla,medPlb,medPlc,medPld] 
+y = [0,1,1,0] 
+plt.plot(x, y,color='orange') 
 
 def medPl(x):
-    a = 2
-    b = 3.9
-    c = 4.1
-    d = 6
+    a = medPla
+    b = medPlb
+    c = medPlc
+    d = medPld
 
     if x < a:
         return 0
@@ -66,30 +62,32 @@ def medPl(x):
 
 
 
-lrgPla = 4
-lrgPlb = 5.5
-lrgPlc = 5.9
-lrgPld = 7
+lrgPla = 4.3
+lrgPlb = 4.7
 
+x = [lrgPla,lrgPlb,7] 
+y = [0,1,1] 
+plt.plot(x, y,color='blue') 
 
 def lrgPl(x):
-    a = 4
-    b = 5.5
-    c = 5.9
-    d = 7
+    a = lrgPla
+    b = lrgPlb
 
-    if x < a:
-        return 0
-    if x >= a and x <= b:
-        return (x - a)/(b - a)
-    if x >= b and x <= c:
+
+    if x > b:
         return 1
-    if x >= c and x <= d:
-        return (d - x)/(d - c)
+    if x >= a and x <= b:
+        return (x-a)/(b-a)
     else:
         return 0
 
 
+
+
+fig = plt.figure(1)
+ax = plt.axes()
+ax.set_xlabel('Pedal Width (Medium and Large)')
+ax.set_ylabel('Degree of Membership')
 
 
 medPwa = 0.8 
@@ -98,11 +96,16 @@ medPwc = 1.5
 medPwd = 2 
 
 
+x = [medPwa,medPwb,medPwc,medPwd] 
+y = [0,1,1,0] 
+plt.plot(x, y,color='orange') 
+
+
 def medPw(x):
-    a = 0.8
-    b = 1.3
-    c = 1.5
-    d = 2
+    a = medPwa
+    b = medPwb
+    c = medPwc
+    d = medPwd
 
     if x < a:
         return 0
@@ -116,27 +119,22 @@ def medPw(x):
         return 0
 
 
-lrgPwa = 1
+lrgPwa = 1.3
 lrgPwb = 1.8
-lrgPwc = 2
-lrgPwd = 3
 
+x = [lrgPwa,lrgPwb,3] 
+y = [0,1,1] 
+plt.plot(x, y,color='blue') 
 
 
 def lrgPw(x):
-    a = 1
-    b = 1.8
-    c = 2.0
-    d = 3
+    a = lrgPwa
+    b = lrgPwb
 
-    if x < a:
-        return 0
-    if x >= a and x <= b:
-        return (x - a)/(b - a)
-    if x >= b and x <= c:
+    if x > b:
         return 1
-    if x >= c and x <= d:
-        return (d - x)/(d - c)
+    if x >= a and x <= b:
+        return (x-a)/(b-a)
     else:
         return 0
 
@@ -169,7 +167,7 @@ pw = X[:, 3:4]
 print('setosa pl min-', min(pl[0:50]), 'max-',max(pl[0:50]) )
 print('versi pl min-', min(pl[50:100]), 'max-',max(pl[50:100]) )
 print('vir pl min-',min(pl[100:150]),  'max-', max(pl[100:150]))
-print('versi pw min-', min(pw[50:100]), 'max-',max(pw[50:100]) )
+print('versi pw min-', min(pw[50:100]), 'max-git',max(pw[50:100]) )
 print('vir pw min-',min(pw[100:150]),  'max-', max(pw[100:150]))
 
 
@@ -192,6 +190,11 @@ rules = [R1,R2,R3]
 findex = 0
 finalResults = np.zeros(len(flowers))
 # rules = [R]
+plotting = True # for individual samples 
+flowers = [[5.1, 3.5, 1.4, 0.2]] # sample setosa 
+flowers = [[5.6, 3. , 4.1, 1.3]] # sample versicolor 
+flowers = [[6.8, 3.2, 5.9, 2.3]] # sample virginica 
+
 for f in flowers:
     findex = findex + 1
     qos = {'pl':f[2], 'pw':f[3],'sl':f[0],'sw':f[1]}
@@ -246,15 +249,44 @@ for f in flowers:
     qots = ['setosa','versicolor','virginica']
     defuzSum = 0
     defuzIndex = 0
+
     for r in rules:
         for val in r:
             if val != 'and' and val != 'or':
                 if list(val.keys())[0] == 'flower':
                     qotIn = qots.index(val['flower']) + 1
                     defuzSum = defuzSum + qotIn*fuzres[defuzIndex]
-                    defuzIndex = defuzIndex + 1
-                    break
+                    ax = plt.axes()
+                    fig.suptitle('IRIS DATASET Pedal Length = {} Pedal Width = {}'.format(qos['pl'], qos['pw']))
+                    ax.set_ylabel('Membership Degree')
+                    ax.set_xlabel('Flower - Setosa=1, Versicolor=2, Virginica=3')
 
+                    if plotting:
+                        fig = plt.figure(2)
+                    else:
+                        defuzIndex = defuzIndex + 1
+
+                    if qotIn == 1 and plotting:
+                        x = [.5,1,1.5] 
+                        y = [0,1,0] 
+                        plt.plot(x, y,color='green' ) 
+                        plt.fill_between(x, fuzres[defuzIndex], color='blue', alpha=.25)
+                        defuzIndex = defuzIndex + 1
+                    if qotIn == 2 and plotting:
+                        x = [1.5,2,2.5] 
+                        y = [0,1,0] 
+                        plt.plot(x, y,color='orange' ) 
+                        plt.fill_between(x, fuzres[defuzIndex], color='blue', alpha=.25)
+                        defuzIndex = defuzIndex + 1
+                    if qotIn == 3 and plotting:
+                        x = [2.5,3,3.5] 
+                        y = [0,1,0] 
+                        plt.plot(x, y,color='blue' ) 
+                        plt.fill_between(x, fuzres[defuzIndex], color='blue', alpha=.25)
+                        defuzIndex = defuzIndex + 1   
+                    break
+    if plotting:
+        break
 
     if sum(fuzres) == 0:
         print(0)
@@ -282,3 +314,4 @@ for i in range(len(finalResults)):
 print((len(finalResults)-errors)/len(finalResults))
 
 
+plt.show()
